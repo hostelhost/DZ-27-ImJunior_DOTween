edd1ef396adc87736271c48ec42f62107589abe8
Илья <106437717+hostelhost@users.noreply.github.com>
2025-12-11 13:28:33 +0300

    Выполнен второй пункт

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private float _duration;
+    [SerializeField] private float _duration = 2f;
+    [SerializeField] private int _repeat = -1;
+    [SerializeField] private float _distance = 5f;
 
     private void Start()
     {
-        transform.DOMove(transform.position, _duration);
+        transform.DOMoveZ(_distance, _duration).SetLoops(_repeat, LoopType.Yoyo).SetEase(Ease.Linear);
     }
 }
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private float _duration = 2f;
+    [SerializeField] private int _repeat = -1;
+    [SerializeField] private Vector3 vector3;
+
+    private void Start()
+    {
+        transform.DORotate(vector3, _duration).SetLoops(_repeat, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+}
